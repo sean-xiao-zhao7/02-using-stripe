07876835832aa71104c11cb9c976bdb6f2544632
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2021-05-28 13:05:38 -0400

    Enable logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,6 +9,9 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const errorController = require("./controllers/error");
 const shopController = require("./controllers/shop");
@@ -49,6 +53,9 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+    flags: "a",
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
@@ -62,7 +69,9 @@ app.use(
         store: store,
     })
 );
-
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(flash());
 
 app.use((req, res, next) => {
